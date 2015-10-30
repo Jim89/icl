64975c15b62479e30e42f0fe7c16ae7a5efa12c4
Jim Leach <jimmy22theave@gmail.com>
2015-10-30 18:43:53 +0000

    Add updated to code for problem set

--- intro_to_da/assignment/p1.R
+++ intro_to_da/assignment/p1.R
@@ -1,35 +1,89 @@
 
 # load packages ----------------------------------------------------------------
+  library(MASS)
+  library(lsa)
   library(igraph)
   library(readxl)
   library(dplyr)
   library(magrittr)
   library(tidyr)
-  library(MASS)
-
+  library(ggplot2)
+  
 # read in the data and set up variables ----------------------------------------
-# read in the data
-groups <- read_excel("./data/BA_Anonymised.xlsx", sheet = "Overview") %>% .[1:12, 1:2]
-people <- read_excel("./data/BA_Anonymised.xlsx", sheet = "Attributes")
-albert_hall_links <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3780")
-workshop_links <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3782")
-weekly_meeting_links <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3781")
-urgent_meeting_links <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3779")
+# read in the metadata about individuals and groups
+  groups                <- read_excel("./data/BA_Anonymised.xlsx", sheet = "Overview") %>% .[1:12, 1:2]
+  people                <- read_excel("./data/BA_Anonymised.xlsx", sheet = "Attributes")
+
+# read in network data
+  albert_hall_links     <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3780")
+  workshop_links        <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3782")
+  weekly_meeting_links  <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3781")
+  urgent_meeting_links  <- read_excel("./data/BA_Anonymised.xlsx", sheet = "relation 3779")
+
+# read in quiz responses
+  guest_list            <- read_excel("./data/BA_Anonymised.xlsx", sheet = "multiple_choice 1056")
+  style                 <- read_excel("./data/BA_Anonymised.xlsx", sheet = "multiple_choice 1055")
+  option1               <- read_excel("./data/BA_Anonymised.xlsx", sheet = "multiple_choice 1054")
+  option2               <- read_excel("./data/BA_Anonymised.xlsx", sheet = "multiple_choice 1053")  
+
+# clean up crap excel imports
+  people %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  albert_hall_links %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  workshop_links %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  weekly_meeting_links %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  urgent_meeting_links %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  guest_list %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  style %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  option1 %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  option2 %<>% apply(2, as.numeric) %>% data.frame() %>% tbl_df()
+  
+# data wrangling and extraction ------------------------------------------------
 
 # define key variables
-back_bone <- 1:57
-dat <- list(albert_hall_links, workshop_links, weekly_meeting_links, urgent_meeting_links)
+  back_bone <- 1:57
+  dat <- list(albert_hall_links, 
+              workshop_links, 
+              weekly_meeting_links, 
+              urgent_meeting_links)
+
+# create base table
+  base <- people %>% 
+          left_join(guest_list, by = c("id" = "rater")) %>% rename(guest_list = item) %>% 
+          left_join(style, by = c("id" = "rater")) %>% rename(style = item) %>% 
+          left_join(option1, by = c("id" = "rater")) %>% rename(option1 = item) %>% 
+          left_join(option2, by = c("id" = "rater")) %>% rename(option2 = item)
 
 
-# data wrangling and extraction ------------------------------------------------
+# get network properties -------------------------------------------------------
+# define formulae to extract properties from a dataframe
+
 # extract in degree centrality
   get_indegrees <- function (data) {
     graph <- graph.data.frame(data, directed = TRUE)
     indegrees <- degree(graph, v = V(graph), mode = "in", loops = FALSE)
     return(indegrees)
   }
-  indegrees <- lapply(seq_along(dat), function(x) get_indegrees(dat[x]))
+  
+# extract betweenness
+  get_betweenness <- function(data) {
+    graph <- graph.data.frame(data, directed = TRUE)
+    betweenness <- betweenness(graph, v = V(graph), directed = TRUE)
+  } 
 
+  
+# get centrality  
+  indegrees <- lapply(seq_along(dat), function(x) get_indegrees(dat[x]))
+  # specific centrality to get popularity points
+  vp <- get_indegrees(albert_hall_links) %>% 
+        data.frame() %>%
+        add_rownames() %>% 
+        setNames(c("id", "vp")) %>% 
+        apply(2, as.numeric) %>% data.frame %>% tbl_df
+
+# get betweenness
+  betweenness <- lapply(seq_along(dat), function(x) get_betweenness(dat[x]))
+
+  
 # extract in centrality in node order (1:57) for easier comparisons
   extract_in_order <- function (degrees_obj) {
    sapply(seq_along(back_bone), function(x) degrees_obj[as.character(x)]) 
@@ -71,4 +125,48 @@ dat <- list(albert_hall_links, workshop_links, weekly_meeting_links, urgent_meet
   fit_pop_to_imp_nb <- glm.nb(implementation ~ popularity, ordered_degrees)  
   fit_pop_to_adv_nb <- glm.nb(advocacy ~ popularity, ordered_degrees)
 
+# develop response to q2 -------------------------------------------------------
+  picks <- data_frame(id = rep(1:57, each = 57), pick = rep(1:57, 57)) %>% 
+            left_join(albert_hall_links, by = c("id" = "rater_id", 
+                                                "pick" = "rated_id")) %>% 
+            rename(popularity = rating) %>% 
+            left_join(workshop_links, by = c("id" = "rater_id", 
+                                             "pick" = "rated_id")) %>% 
+            rename(design = rating) %>%
+            left_join(weekly_meeting_links, by = c("id" = "rater_id", 
+                                             "pick" = "rated_id")) %>% 
+            rename(implementation = rating) %>%
+            left_join(urgent_meeting_links, by = c("id" = "rater_id", 
+                                             "pick" = "rated_id")) %>% 
+            rename(advocacy = rating)
+  
+  picks[is.na(picks)] <- 0
+  
+# extract responses
+  get_similarities <- function (data, id) {
+      data %>% 
+          filter(id == id) %>% 
+          select(-c(1, 2)) %>% 
+          as.matrix() %>% 
+          cosine
+  }
+  
+  similarities <- lapply(seq_along(1:max(picks$id)), 
+                         function(x) get_similarities(picks, x))
+  
+  
+# ------------------------------------------------------------------------------
+# create base table with vp scores
+  # create base table
+  base %<>% left_join(vp, by = C("id" = "id"))
+  write.csv(base, file = "./data/base_table.csv", row.names = F)
+  
+      
+      
+  
+ 
+  
+  
 
+  
+  
