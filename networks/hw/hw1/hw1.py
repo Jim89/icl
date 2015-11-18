3d8b772bf98e960a34ff50480af0f3fabb2e891c
Jim Leach <jimmy22theave@gmail.com>
2015-11-18 19:18:59 +0000

    Add hw1 initial code to repo

---
+++ networks/hw/hw1/hw1.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#%% set working directory
+cd /media/jim/Storage/Documents/gdrive/Imperial/course/networks/hw/hw1
+
+
+#%% load the first network
+data = open("./data/HW1_3.txt", "r")
+network3 = data.read()
+data.close()
+
+# split lines up
+network3 = network3.split("\r\n")
+
+# create list of lists
+n3_vals = []
+for line in range(len(network3)-1):
+    n3_vals.append(network3[line].split(" "))
+    
+
+#%% load the second network
+# load data
+data = open("./data/HW1_4.txt", "r")    
+network4 = data.read()
+data.close()
+
+# split into list
+network4 = network4.split("\r\n")
+
+# create list of lists
+n4_vals = []
+for line in range(len(network4)):
+    n4_vals.append(network4[line].split(" "))
+
+
