7b8d74587cc3af9881f66ad36e0034f9a19fa548
Jim Leach <jimmy22theave@gmail.com>
2015-11-19 14:19:04 +0000

    Add comments to SQL code

--- vldm/assigments/a1/sql_responses/003_EVENTS_WITH_OPERATOR_WITH_T_IN_NAME.sql
+++ vldm/assigments/a1/sql_responses/003_EVENTS_WITH_OPERATOR_WITH_T_IN_NAME.sql
@@ -2,16 +2,18 @@
 FROM
 	EVENT EV
 INNER JOIN
+-- Subquery to return all owners with a 'T' in either their first or last name
 (
 	SELECT DISTINCT OPERATOR_CODE
-	FROM
+	FROM	-- Select all individuals
 		PERSON PERS
-	LEFT JOIN -- SELECT * FROM
+	LEFT JOIN -- Linking table to position code
 		PERSON_POSITION LINK
 	ON PERS.ID = LINK.PERSON_ID
-	LEFT JOIN -- SELECT * FROM
+	LEFT JOIN -- Return the position
 		POSITION POS
 	ON LINK.POSITION_ID = POS.ID
+	-- Take only 'owners' with a 'T' in their first or last name
 	WHERE POS.DESCRIPTION = 'OWNER'
 	AND (FIRST_NAME LIKE '%T%' OR LAST_NAME LIKE '%T%')
 ) OWNERS_T
--- vldm/assigments/a1/sql_responses/005_EVENTS_OCCURING_IN_GREECE.sql
+++ vldm/assigments/a1/sql_responses/005_EVENTS_OCCURING_IN_GREECE.sql
@@ -1,7 +1,9 @@
 ﻿SELECT COUNT(DISTINCT BK.ID) AS EVENTS
 FROM
 	EVENT BK
-LEFT JOIN
+LEFT JOIN -- Return aiport information
 	AIRPORT LK
 ON BK.AIRPORT_ID = LK.ID
 WHERE LK.COUNTRY = 'GR'
+-- (GR = ISO 3166-1 alpha-2 code for Greece)
+
