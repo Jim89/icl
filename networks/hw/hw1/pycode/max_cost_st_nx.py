f594b33d99a99efcfe627a6fb628c505c235498b
Jim Leach <jimmy22theave@gmail.com>
2015-11-23 14:31:21 +0000

    Add networkx implementations to the repo

--- networks/hw/hw1/pycode/max_cost_st_nx.py
+++ networks/hw/hw1/pycode/max_cost_st_nx.py
@@ -5,25 +5,33 @@ Created on Sun Nov 22 15:32:43 2015
 @author: jim
 """
 
-import networkx as nx
+# %% read in the data - use pandas for convenience
 import pandas as pd
 
-data = pd.read_table("../data/HW1_3.txt",
+# read in the data
+data = pd.read_table("./data/HW1_3.txt",
                      sep = " ",
                      header = None,
                      names = ["vx", "vy", "weights"])
-                     
-# initialise graph
-graph = nx.edgelist()
+
+# to find MAX cost spanning tree the (all positive) weights must be multipled
+# by -1:
+data['weights'] = -data['weights']                      
 
 # %%
-for row in range(len(data)):
-    weighted_edge =  tuple(data.iloc[row, ])
-    graph.add_edge(u = weighted_edge[0],
-                   v = weighted_edge[1],
-                   key = None,
-                   attr_dic = None,
-                   weight = weighted_edge[2])
-                   
-tree = nx.minimum_spanning_tree(graph)                  
+import networkx as nx
+
+# convert data frame to graph
+graph = nx.from_pandas_dataframe(data, source = 'vx', target = 'vy', edge_attr = 'weights')
+
+# get the max cost tree                   
+max_cost_st = nx.minimum_spanning_tree(graph, weight = 'weights')             
+
+# re-invert the weights to get the correct sign on the weights
+for edge in range(len(max_cost_st.edges())):
+    edge = max_cost_st.edges()[edge]    
+    vx = edge[0]
+    vy = edge[1]
+    max_cost_st.get_edge_data(vx, vy)['weights'] = - max_cost_st.get_edge_data(vx, vy)['weights']
+    
                      
