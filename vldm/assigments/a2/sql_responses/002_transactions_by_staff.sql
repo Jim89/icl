6f79364d39d2e3d3b5c23e485caed46849107fce
Jim Leach <jimmy22theave@gmail.com>
2015-11-28 10:59:31 +0000

    Add sql queries for assignment 2 to repo

---
+++ vldm/assigments/a2/sql_responses/002_transactions_by_staff.sql
@@ -0,0 +1,29 @@
+﻿/*
+Count	the	number	of	transactions	each	staff	has	been	processing	and	find	
+the	 staff	 member	 (id)	 with	 the	 biggest	 number	 of	 transactions	 and	 also	
+the	staff	member	with	the	biggest	sum	of	the	transaction	value.		
+*/
+
+SELECT
+	 STAFF_ID
+	,COUNT(DISTINCT RENTAL_ID) AS TRANSACTIONS
+	,SUM(AMOUNT) AS TOTAL_TRANSACTION_VALUE
+FROM
+(
+	SELECT
+		 R.RENTAL_ID
+		,R.STAFF_ID
+		-- ,R.STAFF_ID
+		,S.AMOUNT
+	FROM -- SELECT * FROM
+		RENTAL R
+	LEFT JOIN -- SELECT * FROM
+		PAYMENT S
+	ON R.RENTAL_ID = S.RENTAL_ID
+	--WHERE AMOUNT IS NOT NULL
+)_
+GROUP BY
+	STAFF_ID
+
+
+
