53b6b9463f448cce697667ceb34b6ad7002fcced
Jim Leach <jimmy22theave@gmail.com>
2015-12-04 08:56:27 +0000

    Add code to extract power law properties for the graph

--- networks/hw/hw2/code/degree_dist_plot.R
+++ networks/hw/hw2/code/degree_dist_plot.R
@@ -7,14 +7,16 @@ data <- read_csv("./data/HW2_data_degree_dist.csv") %>%
         group_by(degree) %>% 
         tally() %>% 
         rename(nodes = n) %>% 
-        mutate(l_degree = log2(degree),
-               l_nodes = log2(nodes))
+        mutate(l_degree = log(degree),
+               l_nodes = log(nodes))
 
 ggplot(data, aes(x = l_degree, y = l_nodes))+
         geom_point(colour = "firebrick") +
-        xlab(expression(paste("Node Degree", "(Log"[2],")"))) +
-        ylab(expression(paste("Number of Nodes", "(Log"[2],")"))) +
+        xlab(expression(paste("Node Degree", "(Log"["e"],")"))) +
+        ylab(expression(paste("Number of Nodes", "(Log"["e"],")"))) +
         ggtitle("Log-log plot of degree distribution") +
         theme_minimal()
         
-fit <- lm(l_nodes~ l_degree, data = data)        
+fit <- lm(l_nodes~ l_degree, data = data)
+exponent <- fit$coefficients[2]
+a <- exp(fit$coefficients[1])
