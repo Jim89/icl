14ecdcf484c2e823d0d647db24b4ebbd3a9a5880
Jim Leach <jimmy22theave@gmail.com>
2015-12-04 15:51:36 +0000

    Change to karate club network and calcualte several properties

--- networks/hw/hw2/code/2_network_measures.py
+++ networks/hw/hw2/code/2_network_measures.py
@@ -11,15 +11,13 @@ import scipy as sp
 
 # %% read in the data
 
-data = pd.read_table("../data/HW2_data.txt",
-                     sep = "\t",
-                     header = None,
-                     names = ['vx', 'vy'])
+#data = pd.read_table("../data/HW2_data.txt",
+#                     sep = "\t",
+#                     header = None,
+#                     names = ['vx', 'vy'])
 
 # %% convert to graphs
-graph = nx.from_pandas_dataframe(data, 
-                                 source = 'vx',
-                                 target = 'vy')    
+graph = nx.karate_club_graph()
                                  
 # %% get 3 measures
 
@@ -29,23 +27,36 @@ size = graph.size()
  
                           
 # degree distribution
-degrees = nx.degree(graph)
-degrees_data = pd.DataFrame(degrees.items(), columns = ["node", "degree"])
-degrees_data['l_degree'] = np.log(degrees_data['degree'])
-
-degrees_data_grouped = pd.DataFrame(degrees_data.groupby('l_degree').size())
-degrees_data_grouped['l_degree'] = degrees_data_grouped.index
-degrees_data_grouped['l_nodes'] = np.log(degrees_data_grouped[0])
- 
- 
-
-
+#degrees = nx.degree(graph)
+#degrees_data = pd.DataFrame(degrees.items(), columns = ["node", "degree"])
+#degrees_data['l_degree'] = np.log(degrees_data['degree'])
+#
+#degrees_data_grouped = pd.DataFrame(degrees_data.groupby('l_degree').size())
+#degrees_data_grouped['l_degree'] = degrees_data_grouped.index
+#degrees_data_grouped['l_nodes'] = np.log(degrees_data_grouped[0])
 # degrees_data.to_csv("../data/HW2_data_degree_dist.csv")
 
 # density
 density = nx.density(graph)
 
-# 
+# diameter
+diameter = nx.diameter(graph)
+
+# dispersion
+dispersion = nx.dispersion(graph)
+
+# betweenness
+betweenness = nx.betweenness_centrality(graph)
+
+# flow
+flow = nx.centrality.current_flow_betweenness_centrality(graph)
+
+# eigen vector
+eigen = nx.eigenvector_centrality(graph)
+
+# pagerank
+page = nx.pagerank(graph)
+
 
 
                                  
