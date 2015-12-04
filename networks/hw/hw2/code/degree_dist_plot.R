5bbe134adb46027d4d3e3df9b11c35b6bd7accab
Jim Leach <jimmy22theave@gmail.com>
2015-12-04 08:53:24 +0000

    Create log-log plot for degree distribution

--- networks/hw/hw2/code/degree_dist_plot.R
+++ networks/hw/hw2/code/degree_dist_plot.R
@@ -4,6 +4,17 @@ library(ggplot2)
 
 data <- read_csv("./data/HW2_data_degree_dist.csv") %>% 
         select(node, degree) %>% 
-        mutate(log_dist = log2(dist))
+        group_by(degree) %>% 
+        tally() %>% 
+        rename(nodes = n) %>% 
+        mutate(l_degree = log2(degree),
+               l_nodes = log2(nodes))
 
-plot <- qplot(x = -log_dist, y = log_dist, data = data)
+ggplot(data, aes(x = l_degree, y = l_nodes))+
+        geom_point(colour = "firebrick") +
+        xlab(expression(paste("Node Degree", "(Log"[2],")"))) +
+        ylab(expression(paste("Number of Nodes", "(Log"[2],")"))) +
+        ggtitle("Log-log plot of degree distribution") +
+        theme_minimal()
+        
+fit <- lm(l_nodes~ l_degree, data = data)        
