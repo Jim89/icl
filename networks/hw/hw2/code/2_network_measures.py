b2cd2e71a3a7c0ed48a8f7de7d41aacc381a77da
Jim Leach <jimmy22theave@gmail.com>
2015-12-04 15:30:00 +0000

    Add some code to pull out degree dest for network

--- networks/hw/hw2/code/2_network_measures.py
+++ networks/hw/hw2/code/2_network_measures.py
@@ -4,16 +4,19 @@ Created on Tue Dec  1 10:23:20 2015
 
 @author: jim
 """
+import pandas as pd
+import numpy as np
+import networkx as nx
+import scipy as sp
 
 # %% read in the data
-import pandas as pd
+
 data = pd.read_table("../data/HW2_data.txt",
                      sep = "\t",
                      header = None,
                      names = ['vx', 'vy'])
 
 # %% convert to graphs
-import networkx as nx
 graph = nx.from_pandas_dataframe(data, 
                                  source = 'vx',
                                  target = 'vy')    
@@ -28,6 +31,15 @@ size = graph.size()
 # degree distribution
 degrees = nx.degree(graph)
 degrees_data = pd.DataFrame(degrees.items(), columns = ["node", "degree"])
+degrees_data['l_degree'] = np.log(degrees_data['degree'])
+
+degrees_data_grouped = pd.DataFrame(degrees_data.groupby('l_degree').size())
+degrees_data_grouped['l_degree'] = degrees_data_grouped.index
+degrees_data_grouped['l_nodes'] = np.log(degrees_data_grouped[0])
+ 
+ 
+
+
 # degrees_data.to_csv("../data/HW2_data_degree_dist.csv")
 
 # density
