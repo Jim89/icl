8e615371955a3e2e4c06d87365adeab44fb5cae7
Jim Leach <jimmy22theave@gmail.com>
2015-12-06 16:29:46 +0000

    Re-work code to answer provide full question response

--- networks/hw/hw2/code/2_network_measures.py
+++ networks/hw/hw2/code/2_network_measures.py
@@ -4,17 +4,9 @@ Created on Tue Dec  1 10:23:20 2015
 
 @author: jim
 """
-import pandas as pd
-import numpy as np
 import networkx as nx
-import scipy as sp
-
-# %% read in the data
-
-#data = pd.read_table("../data/HW2_data.txt",
-#                     sep = "\t",
-#                     header = None,
-#                     names = ['vx', 'vy'])
+import community as cty
+import pandas as pd
 
 # %% convert to graphs
 graph = nx.karate_club_graph()
@@ -27,14 +19,7 @@ size = graph.size()
  
                           
 # degree distribution
-#degrees = nx.degree(graph)
-#degrees_data = pd.DataFrame(degrees.items(), columns = ["node", "degree"])
-#degrees_data['l_degree'] = np.log(degrees_data['degree'])
-#
-#degrees_data_grouped = pd.DataFrame(degrees_data.groupby('l_degree').size())
-#degrees_data_grouped['l_degree'] = degrees_data_grouped.index
-#degrees_data_grouped['l_nodes'] = np.log(degrees_data_grouped[0])
-# degrees_data.to_csv("../data/HW2_data_degree_dist.csv")
+degrees = nx.degree(graph)
 
 # density
 density = nx.density(graph)
@@ -57,6 +42,62 @@ eigen = nx.eigenvector_centrality(graph)
 # pagerank
 page = nx.pagerank(graph)
 
+data = pd.DataFrame(degrees.values(), columns = ['degree'])
+data['betweenness'] = betweenness.values()
+data['flow'] = flow.values()
+data['eigen'] = eigen.values()
+data.to_csv('../data/measures.csv')
+
+# %% find communites
+partition_estimate = cty.best_partition(graph)
+mod = cty.modularity(partition_estimate, graph)
+
+# read in ground truth
+partitions = pd.read_table('../data/karate_truth.txt', sep = '\t', header = 0)
+mod_ground_truth = cty.modularity(partitions.to_dict()['partition'], graph)
+
+# attach estimate and add comparison
+partitions['estimate'] = pd.Series(partition_estimate.values())+1
+partitions['compare'] = partitions['partition'] == partitions['estimate']
+
+# write data to csv
+partitions.to_csv("../data/partitions.csv")
+
+# %% evaluate
+# chose density 
+
+# ground truth ----------------------------------------------------------------
+ground_first_group = nx.karate_club_graph()
+ground_first_group.remove_nodes_from(partitions[partitions.partition != 1].node-1)
+
+ground_second_group = nx.karate_club_graph()
+ground_second_group.remove_nodes_from(partitions[partitions.partition != 2].node-1)
+
+# densities
+g1_dens = nx.density(ground_first_group)
+g2_dens = nx.density(ground_second_group)
 
 
+# estimates -------------------------------------------------------------------
+estimate_group1 = nx.karate_club_graph()
+estimate_group1.remove_nodes_from(partitions[partitions.estimate != 1].node-1)
+
+estimate_group2 = nx.karate_club_graph()
+estimate_group2.remove_nodes_from(partitions[partitions.estimate != 2].node-1)
+
+estimate_group3 = nx.karate_club_graph()
+estimate_group3.remove_nodes_from(partitions[partitions.estimate != 3].node-1)
+
+estimate_group4 = nx.karate_club_graph()
+estimate_group4.remove_nodes_from(partitions[partitions.estimate != 4].node-1)
+
+# get densities
+e1_dens = nx.density(estimate_group1)
+e2_dens = nx.density(estimate_group2)
+e3_dens = nx.density(estimate_group3)
+e4_dens = nx.density(estimate_group4)
+
+
+    
+                                 
                                  
