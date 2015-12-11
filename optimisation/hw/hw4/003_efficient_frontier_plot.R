3ddd31082d360f853c3a60d96eee8ef683c5fb85
Jim Leach <jimmy22theave@gmail.com>
2015-12-11 17:33:48 +0000

    Add some code to plot the efficient frontier

--- optimisation/hw/hw4/003_efficient_frontier_plot.R
+++ optimisation/hw/hw4/003_efficient_frontier_plot.R
@@ -1,5 +1,8 @@
-# plot efficient frontier
-ggplot(sims, aes(x = variance, y = returns)) +
-    geom_point() +
-    xlab("Risk") +
-    ylab("Return")
+# load necessary packages
+ggplot(results, aes(x = variance, y = returns)) + 
+    geom_point(alpha = .35, color = "steelblue") +
+    ggtitle("Efficient Frontier") +
+    labs(x="Portfolio Risk", y = "Portfoli Return") +
+    theme(panel.background=element_rect(fill=eallighttan),
+          text=element_text(color = "black"),
+          plot.title=element_text(size=24, color="black"))
