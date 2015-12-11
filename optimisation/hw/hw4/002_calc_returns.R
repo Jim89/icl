0c72c761982e43a88172edb9348a2f57d3a002f0
Jim Leach <jimmy22theave@gmail.com>
2015-12-11 17:33:32 +0000

    Add loop to calculate returns at multiple thresholds

--- optimisation/hw/hw4/002_calc_returns.R
+++ optimisation/hw/hw4/002_calc_returns.R
@@ -45,17 +45,58 @@ avg_returns <- apply(returns_data, 2, mean)
 # calcuate return covariances
 covars <- cov(returns_data)
 
+# attempt to solve minimisation problem ----------------------------------------
+# will use general form of quadratic program see here:
+# http://www.rinfinance.com/RinFinance2009/presentations/yollin_slides.pdf
+# for details
 
+# number of stocks
+n <- ncol(returns_data)
 
+# A-matrix - defines constraints - include diagonal for no short selling
+Amat <- cbind(1, diag(n))
 
+# b-vector - defines constraint RHS value(s)
+bvec <- c(1, rep(0, n))
 
+# set parameters
+risk_upper_bound <- 1
+risk_increment <- 0.001
 
+# combinations for return risk premium
+premiums <- seq(from = 0, to = risk_upper_bound, by = risk_increment)
 
+# initialse tbl to hold solution and 2 cols for variance and return
+results <- matrix(nrow = length(premiums), ncol = n + 2) 
 
+# set the names on the results tbl
+colnames(results) <- c(stocks, "variance", "returns")
 
+# counter to keep track of iterations
+iter <- 1
+for (i in premiums) {
+    # set the acceptable returns threshold - some fraction of the mean returns
+    # that was calculated previously
+    dvec <- colMeans(returns_data) * i 
+    
+    # solve the system with that minimal value of acceptable returns
+    sol <- solve.QP(covars, dvec = dvec, Amat = Amat, bvec = bvec, meq = 1)
+    
+    # extract the solution, i.e the choices 
+    choices <- sol$solution
+    
+    # get variance and return
+    var <- t(choices) %*% covars %*% choices
+    ret <- t(avg_returns) %*% choices
+    
+    # save results to the tbl
+    results[iter, 1:n] <- choices
+    results[iter, n+1] <- var
+    results[iter, n+2] <- ret
+    
+    # update the iteration counter
+    iter <- iter + 1
+    
+}
 
-
-
-
-
-
+results <- data.frame(results)
