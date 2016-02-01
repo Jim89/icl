e72141873fc12cb36113fa1ed42ad6386cfe8990
Jim Leach <jimmy22theave@gmail.com>
2016-02-01 17:29:58 +0000

    Add code to finish scrape and push results to directory

--- workforce/hw/group/r/01_scrape_people.R
+++ workforce/hw/group/r/01_scrape_people.R
@@ -1,6 +1,7 @@
 # 0 - set up environment -------------------------------------------------------
 library(rvest)
 library(magrittr)
+library(stringr)
 
 # 1 - define helper functions --------------------------------------------------
 ExtractLinks <-
@@ -12,10 +13,20 @@ ExtractLinks <-
       html_attr("href")
   }
 
-# function to extract station name
+# function to extract name
+ExtractName <- function(url) {
+  # takes a URL and uses the CSS selector provided by SelectorGadget widget to
+  # extract the page header - in this case a researcher's name
+  url %>%
+    read_html() %>%
+    html_nodes(".ac_info") %>%
+    html_text()
+}
+
+# function to extract summary
 ExtractContent <- function(url) {
   # takes a URL and uses the CSS selector provided by SelectorGadget widget to
-  # extract the first header, which in this case is the wikipedia page title
+  # extract the content with sidebar - in this case a researcher's summary
   url %>%
     read_html() %>%
     html_nodes(".contentAndSidebar") %>%
@@ -23,6 +34,7 @@ ExtractContent <- function(url) {
 }
 
 # 2 - set up scrape for people -------------------------------------------------
+ptm <- proc.time()
 # Set base URL
 url <- "http://wwwf.imperial.ac.uk/business-school/people/"
 
@@ -33,6 +45,33 @@ idx <- grep('uk/people/', links)
 # select only those that are a person
 links <- links[idx]
 
-ptm <- proc.time()
+# loop over links and grab the summaries
 people_summaries <- lapply(links, ExtractContent)
-proc.time() - ptm
+
+# loop over links and grab names
+# people_names <- lapply(links, ExtractName)
+
+# 3 write data out to files ----------------------------------------------------
+for(i in seq_along(links)) {
+  # split up URL to get indviduals' name
+  split_up <- unlist(str_split(links[i], '/'))
+  last_item <- length(split_up)
+  individ <- split_up[last_item]
+  individ <- gsub("\\.", "_", individ)
+  
+  # make the necessary directory
+  if(!dir.exists("./data/researchers_scraped")) {
+    dir.create("./data/researchers_scraped")
+    }
+  
+  # make the file
+  file <- paste0("./data/researchers_scraped/", individ, ".txt")
+  file.create(file)
+  
+  # clean up scraped data slightly
+  people_summaries[i] <- str_trim(gsub("\\r|\\n|\\t", "", people_summaries[i]))
+  
+  writeLines(people_summaries[[i]], file)
+}
+
+print(paste0("Complete. Took: ", ceiling(proc.time()[3] - ptm[3]), " seconds"))
