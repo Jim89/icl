f9f06ae8785e54f2c99157b23911f49f6be209f7
Jim Leach <jimmy22theave@gmail.com>
2016-02-05 12:52:02 +0000

    Add further cleaning codes

--- workforce/workshops/w03/01_workshop3.py
+++ workforce/workshops/w03/01_workshop3.py
@@ -62,21 +62,69 @@ d3_reshaped['pnum_list'] = pnum_list
                              
 
 # %% Step 2 - create structure to track inventor projects
-d3_inventor_projects = pd.DataFrame(d3_reshaped.groupby('inv_num')['pnum_list'].sum()
+d3_inventor_projects = pd.DataFrame(d3_reshaped.groupby('inv_num')['pnum_list'].sum())
 d3_inventor_projects.reset_index(level = 0, inplace = True)
 d3_inventor_projects.columns = ['inv_num', 'pnum_list']
 
-# %% Step 3 - Define all indices
 
 # %% Step 4 - Get inventors on more than one project
 d3_inventor_projects['num_projects'] = [len(x) for x in d3_inventor_projects.pnum_list]
 multiproj_invs = d3_inventor_projects[d3_inventor_projects.num_projects > 1]
-prop = len(multiproj_invs)/float(len(d3_inventor_projects))
+prop_proj = len(multiproj_invs)/float(len(d3_inventor_projects))
 
+# %% Step 5 - Get inventors at more than one firm
+d3_firm_to_invs = pd.concat([d3.firm,
+                             d3.pnum,
+                             d3.year,
+                             d3.inv_num.apply(lambda y: pd.Series(y.split(';')))], 
+                             axis = 1)
+                             
+d3_firm_to_inv_melt = pd.melt(d3_firm_to_invs, 
+                             id_vars = ['firm', 'pnum', 'year'], 
+                             value_vars = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,25,25],
+                             value_name = 'inv_num')
+d3_reshaped_firms = d3_firm_to_inv_melt.drop('variable', axis = 1)
+d3_reshaped_firms = d3_reshaped_firms.sort_values(['inv_num', 'pnum', 'firm'], ascending = [1, 1, 1])
+d3_reshaped_firms['firm_list'] = [[x] for x in d3_reshaped_firms.firm]
+    
+d3_inventor_firms = pd.DataFrame(d3_reshaped_firms.groupby('inv_num')['firm_list'].sum())    
+d3_inventor_firms.reset_index(level = 0, inplace = True)
+d3_inventor_firms.columns = ['inv_num', 'firm_list']
 
-    
-    
-    
+d3_inventor_firms['num_unique_firms'] = [len(set(x)) for x in d3_inventor_firms.firm_list]
+
+multi_firm_invs = d3_inventor_firms[d3_inventor_firms.num_unique_firms > 1]
+
+prop_firm = len(multi_firm_invs)/float(len(d3_inventor_firms))
+
+# %% Step 6 - get inventor performance over time
+d3_perf_to_invs = pd.concat([d3[' performance'],
+                             d3.pnum,
+                             d3.inv_num.apply(lambda y: pd.Series(y.split(';')))], 
+                             axis = 1)
+
+d3_perf_to_invs['perf'] = [[x] for x in d3_perf_to_invs[' performance']]
+
+                             
+d3_perf_to_inv_melt = pd.melt(d3_firm_to_invs, 
+                             id_vars = ['perf', 'pnum'], 
+                             value_vars = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,25,25],
+                             value_name = 'inv_num')
+d3_reshaped_perfs = d3_perf_to_inv_melt.drop('variable', axis = 1)
+
+# %% Step 7 - look at experience
+d3_inv_exp = pd.DataFrame(d3_reshaped_firms.groupby('inv_num')['year'].apply(lambda x: max(x) - min(x)))
+d3_inv_exp.reset_index(level = 0, inplace = True)
+
+# %% Step 10 - Create combined data set
+d3_combined = pd.merge(d3_inventor_firms,
+                       d3_inventor_projects,
+                       left_on = 'inv_num',
+                       right_on = 'inv_num',
+                       how = 'left').merge(d3_inv_exp,
+                                            left_on = 'inv_num',
+                                            right_on = 'inv_num',
+                                            how = 'left')
 
 
 
