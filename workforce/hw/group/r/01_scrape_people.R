fafce18322dfa47d086a00a7a20d238859e650fe
Jim Leach <jimmy22theave@gmail.com>
2016-02-08 10:55:50 +0000

    Add code to get clean name as well as text summary

--- workforce/hw/group/r/01_scrape_people.R
+++ workforce/hw/group/r/01_scrape_people.R
@@ -13,6 +13,21 @@ ExtractLinks <-
       html_attr("href")
   }
 
+ExtractLinksText <-
+  function(url) {
+    # takes a URL and extracts all links text from it
+    title <- url %>%
+              read_html() %>%
+              html_nodes("a") %>%
+              html_attr("title")
+    link <-     url %>%
+                read_html() %>%
+                html_nodes("a") %>%
+                html_attr("href")
+    title_link <- paste(title, link, sep = " : ")
+    return(title_link)
+  }
+
 # function to extract name
 ExtractName <- function(url) {
   # takes a URL and uses the CSS selector provided by SelectorGadget widget to
@@ -45,11 +60,31 @@ idx <- grep('uk/people/', links)
 # select only those that are a person
 links <- links[idx]
 
+# Grab names of individuals
+title_links <- ExtractLinksText(url)
+idx <- grep('uk/people/', title_links)
+title_links <- title_links[idx]
+names <- sapply(seq_along(title_links), function(x) unlist(str_split(title_links[[x]], " : "))[1])
+names[5] <- "Anandalingam Anand"
+names[12] <- "Bruno Biais"
+names[21] <- "Claudia Custodio"
+names[27] <- "Andreas Eisingerich"
+names[34] <- "Joachin Geske"
+names[48] <- "Andrei Kirilenko"
+names[55] <- "Ian Mackenzie"
+names[61] <- "Edgar Meyer"
+names[76] <- "Eliana Barrenho"
+names[82] <- "Maria Rosa Sanchis-Guarner Herrero"
+names[83] <- "Franco Sassi"
+names[89] <- "Konstantina Spanaki"
+names[96] <- "Angelos Tsoukalas"
+names[99] <- "Antoinne Vernet"
+names[100] <- "Anu Wadhawa"
+names[105] <- "George Yip"
+names <- data.frame(names, file = rep(NA, length(names)))
+  
 # loop over links and grab the summaries
 people_summaries <- lapply(links, ExtractContent)
-
-# loop over links and grab names
-# people_names <- lapply(links, ExtractName)
 
 # 3 write data out to files ----------------------------------------------------
 for(i in seq_along(links)) {
@@ -58,6 +93,7 @@ for(i in seq_along(links)) {
   last_item <- length(split_up)
   individ <- split_up[last_item]
   individ <- gsub("\\.", "_", individ)
+  names[i, "file"] <- individ
   
   # make the necessary directory
   if(!dir.exists("./data/researchers_scraped")) {
@@ -74,4 +110,5 @@ for(i in seq_along(links)) {
   writeLines(people_summaries[[i]], file)
 }
 
+write.csv(names, "./data/researchers_scraped/names.csv", row.names = FALSE) 
 print(paste0("Complete. Took: ", ceiling(proc.time()[3] - ptm[3]), " seconds"))
