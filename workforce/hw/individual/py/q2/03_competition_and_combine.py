b8916f13a34c11ef4fce2f869a5866ba00dea1a3
Jim Leach <jimmy22theave@gmail.com>
2016-02-15 17:09:14 +0000

    Add code to write to csv

--- workforce/hw/individual/py/q2/03_competition_and_combine.py
+++ workforce/hw/individual/py/q2/03_competition_and_combine.py
@@ -10,7 +10,12 @@ import pandas as pd
 import numpy as np
 
 # %% Step 1 - Get the data
+base = pd.read_csv("../../../../data/outputs/q1b_base.csv")
 sims = pd.read_csv("../../../../data/outputs/sims.csv")
+eth = pd.read_csv("../../../../data/outputs/firm_eth.csv")
+d2 = pd.read_csv("../../../../data/D2_firm_level_data.csv")
+d2.columns = ['firm', 'emps', 'sales', 'rnd']
+
 
 # %% Step 2 - Aggregate similarity data
 # 1. calculate mean competition for all firms in D1 data
@@ -28,3 +33,16 @@ closest_comps_grouped.columns = ['firm', 'comps']
 
 # 4. find number of competitors
 closest_comps_grouped['n_comp'] = closest_comps_grouped.comps.apply(lambda x: len(x.split(',')))
+
+# %% Step 3  - combine
+# Select only certain fields for joining purposes later
+eth.drop(['ethnicity', 'cntry'], axis = 1, inplace = True)
+closest_comps_grouped.drop('comps', axis = 1, inplace = True)
+
+q1b = pd.merge(base, eth, how = "left")
+q1b = pd.merge(q1b, d2, how = "left")
+q1b = pd.merge(q1b, mean_comps, how = "left")
+q1b = pd.merge(q1b, closest_comps_grouped, how = "left")
+
+# Push to csv
+q1b.to_csv("../../../../data/outputs/q1b.csv", index = False)
