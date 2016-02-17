bd2630e71f3b07096db96fc66631eef7e25b8f02
Jim Leach <jimmy22theave@gmail.com>
2016-02-17 12:04:52 +0000

    Clean and structure code

--- workforce/hw/individual/r/q2/q2_regressions.R
+++ workforce/hw/individual/r/q2/q2_regressions.R
@@ -1,29 +1,22 @@
+# Step 0 - load packages -------------------------------------------------------
 library(readr)
 library(dplyr)
 library(magrittr)
-library(caret)
-library(randomForest)
+library(logistf)
 
+# Step 1 - load and clean data -------------------------------------------------
+# load
 q1b <- read_csv("./data/outputs/q1b.csv")
+
+# transform certain columns
 q1b$moved_to %<>% as.logical()
-q1b$moved_to %<>% as.numeric() %>% as.factor()
 q1b$n_comp %<>% as.numeric()
 
-q1b_clean <- q1b[complete.cases(q1b), ]
+# Step 1 - fit models ----------------------------------------------------------
+# Standard logit
+fit <- glm(moved_to ~ ., data = q1b %>% select(-firm, -inv), family = "binomial")
 
+# Firth logit (penalised likelihood)
+fit_f <- logistf(moved_to ~ ., data = q1b %>% select(-firm, -inv))
 
-
-idx <- createDataPartition(q1b_clean$moved_to, p = 0.75, list = F)
-train <- q1b_clean[idx, ]
-test <- q1b_clean[-idx, ]
-
-fit <- glm(moved_to ~ ., data = train %>% select(-firm, -inv), family = "binomial")
-fit_rf <- randomForest(factor(moved_to) ~ ., data = train %>% select(-firm, -inv),
-                       ntree = 10, importance = T)
-
-logit_pred <- predict(fit, test)
-rf_pred <- predict(fit_rf, test)
-
-confMat_logit <- confusionMatrix(as.factor(logit_pred), test$moved_to)
-confMat_rf <- confusionMatrix(rf_pred, test$moved_to)
 
