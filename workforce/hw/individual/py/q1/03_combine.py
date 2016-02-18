dabf5964038547eedf6f5b0e6233d211b538a903
Jim Leach <jimmy22theave@gmail.com>
2016-02-18 08:52:31 +0000

    Add in inventor performance

--- workforce/hw/individual/py/q1/03_combine.py
+++ workforce/hw/individual/py/q1/03_combine.py
@@ -9,9 +9,13 @@ Created on Wed Feb 17 14:22:04 2016
 import pandas as pd
 
 # %%
-d3_eth = pd.read_csv("../../../../data/d3_patents_to_eth.csv")
-collab = pd.read_csv("../../../../data/outputs/collab1.csv")
+d3_eth = pd.read_csv("../../../../data/D3_patents_to_eth.csv")
+collab1 = pd.read_csv("../../../../data/outputs/collab1.csv")
+collab2 = pd.read_csv("../../../../data/outputs/collab2.csv")
+pat_inv_perf = pd.read_csv("../../../../data/outputs/paths_to_inv_perf.csv")
 
 # %%
-q1a = pd.merge(d3_eth, collab)
+q1a = pd.merge(d3_eth, collab1)
+q1a = pd.merge(q1a, collab2)
+q1a = pd.merge(q1a, pat_inv_perf)
 q1a.to_csv("../../../../data/outputs/q1a.csv", index = False)
