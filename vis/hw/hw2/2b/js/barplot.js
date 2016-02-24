8a06fccd4ff97eacffcdd8ee0f7a07bfe4f5c652
Jim Leach <jimmy22theave@gmail.com>
2016-02-24 10:31:24 +0000

    Move back to vertical alignment - need to realign!

--- vis/hw/hw2/2b/js/barplot.js
+++ vis/hw/hw2/2b/js/barplot.js
@@ -2,22 +2,41 @@
 Generate a bar plot
 **/
 function barplot(data) {
-   x.domain([0, d3.max(data, function(d) { return d.values; })]);
+// Set up x and y domains
+  x.domain(data.map(function(d) { return d.key; }));
+  y.domain([0, d3.max(data, function(d) { return d.values; })]);
 
-  chart.attr("height", barHeight * data.length);
+// Add X axis
+  svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .selectAll("text")
+        .attr("y", 0)
+        .attr("x", -150)
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(-90)")
+        .style("text-anchor", "start")
 
-  var bar = chart.selectAll("g")
+// Add Y axis
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Total Medals");
+
+
+// Add the bars (i.e. the data!)
+  svg.selectAll(".bar")
       .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-  bar.append("rect")
-      .attr("width", function(d) { return x(d.values); })
-      .attr("height", barHeight - 1);
-
-  bar.append("text")
-      .attr("x", function(d) { return x(d.values) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.values; });
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.key); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.values); })
+      .attr("height", function(d) { return height - y(d.values); });
 } 
