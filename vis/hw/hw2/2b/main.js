83617cea4f4b7ef319ead8546f99910e1085a6de
Jim Leach <jimmy22theave@gmail.com>
2016-02-24 09:28:56 +0000

    Add some filtering to view tidier data (for now)

--- vis/hw/hw2/2b/main.js
+++ vis/hw/hw2/2b/main.js
@@ -11,7 +11,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
     height = 500 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+    .rangeRoundBands([0, width], .15);
 
 var y = d3.scale.linear()
     .range([height, 0]);
@@ -51,12 +51,18 @@ d3.tsv('./data/MedalData1.csv', function(metaldata1) {
                 .key(function(d) { return d.Athlete })
                 .rollup(function(leaves) { return leaves.length; })
                 .entries(metaldata1);
-    
-  //render the subset    
-  // updateTable(table1, rolled_up);
+
+             
+
+  var filtered = rolled_up.filter(function(d) { return d.values >= 4; });
 
   // render barplot
-  barplot(rolled_up);
+  barplot(filtered);              
+    
+  //render the subset    
+  // updateTable(table1, filtered);
+
+
 });
 
 function type(d) {
