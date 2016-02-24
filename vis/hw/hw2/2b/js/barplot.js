047c706d48a0c2cdf1dcb1f8981735a26b000aac
Jim Leach <jimmy22theave@gmail.com>
2016-02-24 09:29:39 +0000

    Change y-axis label, and add rotated x-axis labels (ovelap with plot - need to change)

--- vis/hw/hw2/2b/js/barplot.js
+++ vis/hw/hw2/2b/js/barplot.js
@@ -2,14 +2,24 @@
 Generate a bar plot
 **/
 function barplot(data) {
+// Set up x and y domains
   x.domain(data.map(function(d) { return d.key; }));
   y.domain([0, d3.max(data, function(d) { return d.values; })]);
 
-svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
 
+// Add X axis
+  svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+      .selectAll("text")
+        .attr("y", 0)
+        .attr("x", -50)
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(-90)")
+        .style("text-anchor", "start")
+
+// Add Y axis
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
@@ -18,8 +28,9 @@ svg.append("g")
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Frequency");
+      .text("Total Medals");
 
+// Add the bars (i.e. the data!)
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
