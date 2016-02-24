34468150ad6579fab02f99b40001e34aca5e5cff
Jim Leach <jimmy22theave@gmail.com>
2016-02-24 10:11:25 +0000

    Revert back to (entirely static) horizontal chart - need to add axes and labels

--- vis/hw/hw2/2b/js/barplot.js
+++ vis/hw/hw2/2b/js/barplot.js
@@ -2,41 +2,22 @@
 Generate a bar plot
 **/
 function barplot(data) {
-// Set up x and y domains
-  x.domain(data.map(function(d) { return d.key; }));
-  y.domain([0, d3.max(data, function(d) { return d.values; })]);
+   x.domain([0, d3.max(data, function(d) { return d.values; })]);
 
-// Add X axis
-  svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-      .selectAll("text")
-        .attr("y", 0)
-        .attr("x", -150)
-        .attr("dy", ".35em")
-        .attr("transform", "rotate(-90)")
-        .style("text-anchor", "start")
+  chart.attr("height", barHeight * data.length);
 
-// Add Y axis
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Total Medals");
-
-
-// Add the bars (i.e. the data!)
-  svg.selectAll(".bar")
+  var bar = chart.selectAll("g")
       .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.key); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.values); })
-      .attr("height", function(d) { return height - y(d.values); });
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.values); })
+      .attr("height", barHeight - 1);
+
+  bar.append("text")
+      .attr("x", function(d) { return x(d.values) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.values; });
 } 
--- vis/hw/hw2/2b/main.js
+++ vis/hw/hw2/2b/main.js
@@ -6,50 +6,14 @@ table1.append('caption').text('Dataset1');
 table1.append('thead').append('tr');
 table1.append('tbody');    
 
-var margin = {top: 20, right: 20, bottom: 150, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var width = 420,
+    barHeight = 20;
 
-
-var x = d3.scale.ordinal()
-        .rangeRoundBands([0, width], .15);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .tickSize(0, 0)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")  
-
-
-/*
 var x = d3.scale.linear()
-        .range([0, width]);
+    .range([0, width]);
 
-var y = d3.scale.ordinal()
-        .rangeRoundBands([height, 0], .15);
-
-var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("top");
-
-var yAxis = d3.svg.axis()
-            .scale(y)
-            .tickSize(0, 0)
-            .orient("left");
-*/
-
-
-var svg = d3.select("body").append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");    
+var chart = d3.select(".chart")
+    .attr("width", width);   
 
 // Step 1 - read in external data ----------------------------------------------------
 d3.tsv('./data/MedalData1.csv', function(metaldata1) {
