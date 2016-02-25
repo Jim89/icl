c4f4207fd757242759f4a1b6ec8e019e89f0cc51
Jim Leach <jimmy22theave@gmail.com>
2016-02-25 10:27:15 +0000

    Update barplot with colours for gender and legend

--- vis/hw/hw2/2b/js/barplot.js
+++ vis/hw/hw2/2b/js/barplot.js
@@ -2,55 +2,20 @@
 Generate a bar plot
 **/
 function barplot(data) {
-/*
+
 // Set up x and y domains
-  x.domain(data.map(function(d) { return d.key; }));
-  y.domain([0, d3.max(data, function(d) { return d.values; })]);
+x_bar.domain([0, d3.max(data, function(d) { return d.Medals; })]);
+y_bar.domain(data.map(function(d) { return d.Athlete; }));
+
+// Set colous labels
+data.forEach(function(d) {
+  d.color_bar = color(d.Gender);
+})  
 
 // Add X axis
   svg.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-      .selectAll("text")
-        .attr("y", 0)
-        .attr("x", -150)
-        .attr("dy", ".35em")
-        .attr("transform", "rotate(-90)")
-        .style("text-anchor", "start")
-
-// Add Y axis
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Total Medals");
-
-
-// Add the bars (i.e. the data!)
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.key); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.values); })
-      .attr("height", function(d) { return height - y(d.values); });
-*/
-
-// Set up x and y domains
-x.domain([0, d3.max(data, function(d) { return d.Medals; })]);
-y.domain(data.map(function(d) { return d.Athlete; }));
-
-
-// Add X axis
-  svg.append("g")
-        .attr("class", "x axis")
-        .call(xAxis)
+        .call(xAxis_bar)
       .selectAll("text")
         .attr("y", -2)
         .attr("x", -3)
@@ -60,7 +25,7 @@ y.domain(data.map(function(d) { return d.Athlete; }));
 // Add Y axis
   svg.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
+      .call(yAxis_bar)
 
 // Add x-axis label
   svg.append("text")
@@ -70,19 +35,41 @@ y.domain(data.map(function(d) { return d.Athlete; }));
     .attr("x", width/2)
     .text("Total Medals")
   
-    
-
 // Add the bars (i.e. the data!)
   svg.selectAll(".bar")
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
       .attr("x", 0)
-      .attr("width", function(d) { return x(d.Medals)})
-      .attr("y", function(d) { return y(d.Athlete); })
-      .attr("height", y.rangeBand())
+      .attr("width", function(d) { return x_bar(d.Medals)})
+      .attr("y", function(d) { return y_bar(d.Athlete); })
+      .attr("height", y_bar.rangeBand())
+      .style("fill", function(d) { return d.color_bar; })
       .on("mouseover", tip.show)
-      .on("mouseout", tip.hide);       
+      .on("mouseout", tip.hide);  
+
+// Add legend
+var legend = svg.selectAll(".legend")
+        .data(color.domain())
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+legend.append("rect")
+  .attr("x", width - 18)
+  .attr("y", height - 100)  
+  .attr("width", 18)
+  .attr("height", 18)
+  .style("fill", color);
+
+legend.append("text")
+  .attr("x", width - 24)
+  .attr("y", height - 90)
+  .attr("dy", ".35em")
+  .style("text-anchor", "end")
+  .text(function(d) { return d; });           
+
+  
 } 
 
 
