2d13c7c12a97aaed52d8aa6332f50c87658b6296
Jim Leach <jimmy22theave@gmail.com>
2016-03-03 17:33:03 +0000

    Add matlab plotting code to scratch

--- ml/hw/hw1/ml/scratchPad.m
+++ ml/hw/hw1/ml/scratchPad.m
@@ -1,17 +1,19 @@
 %% 
 clc;
+% rng(19891110);
 kernels = {'gaussian', 'polynomial', 'linear'};
-slacks = linspace(0.1, 1);
+slacks = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 1e1, 1e2, 1e3, 1e4, 1e5];
+
+
 
 losses_svm = zeros(size(slacks, 2), size(kernels, 2));
 for i = 1:length(slacks)
   for j = 1:3
     % Fit the model
-    fitSVM = fitcsvm(input, output, ...
-                     'Standardize', true,...
+    fitSVM = fitcsvm(train_input, train_output, ...
+                     'Standardize', 1,...
                      'BoxConstraint', slacks(i),...
                      'KernelFunction', kernels{j},...
-                     'KernelScale', 'auto',...
                      'CrossVal', 'on');
 
     % Compute k-fold loss
@@ -29,8 +31,11 @@ losses_svm(losses_svm == 0) = NaN;
 [I_row, I_col] = ind2sub(size(losses_svm), idx);
 
 % Set optimal k and distance
-slack = I_row;
+slack = slacks(I_row);
 kern = kernels{I_col};
+
+loss_svm_cln = dataset({losses_svm 'gaussian', 'polynomial', 'linear'});
+export(loss_svm_cln, 'File', '../data/created/svm_loss.csv', 'Delimiter', ',');
 
 disp(slack)
 disp(kern)
@@ -78,7 +83,24 @@ disp(dist)
 % export(loss_cln, 'File', '../data/created/knn_loss.csv', 'Delimiter', ',');
 
 
+%% 
+%% Create histograms
+cols = size(input, 2);
+for i = 1:cols
+    filename = strcat('../images/input_hists/input_col_', num2str(i));
+    histogram(input((output==0), i), 'facecolor', 'r');
+    hold on;
+    histogram(input((output==1), i), 'facecolor', 'b');
+    hold off;
+    legend('output0','output1')
+    print(filename, '-dpng');
+end    
 
+%% Check data and assess class distributions
+nan_input = sum(any( isnan(input), 2 )); % Rows with NaN
+nan_output = sum(any( isnan(output), 2 )); % Rows with NaN
+
+histogram(output); % Visualise class distributions
 
 
 
