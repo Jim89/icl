228077bf8a9d0b0cf048a7a9a6e81c40d32c85f5
Jim Leach <jimmy22theave@gmail.com>
2016-03-03 13:24:21 +0000

    Add comments and clean code up slightly

--- ml/hw/hw1/ml/functions/TrainClassifier2.m
+++ ml/hw/hw1/ml/functions/TrainClassifier2.m
@@ -1,12 +1,18 @@
 function parameters = TrainClassifier2(input, output)
 
+% Set max k
+max_k = 30;
+
 % Set up distance metrics
 distances = {'cityblock', 'chebychev', 'euclidean', 'minkowski'};
+
+% Set up losses matrix to fill in
+losses = zeros(max_k, size(distances, 2));
 
 % Iterate over distance metrics
 for j = 1:4
   % Iterate over values of k to find the best model
-  for i = 1:30
+  for i = 1:max_k
     fitknn = fitcknn(input, output, ...
                      'NumNeighbors', i,...
                      'CrossVal', 'on',...
@@ -17,9 +23,14 @@ for j = 1:4
   end
 end;
 
-[min, idx] = min(losses(:));
+% Set up case for 0 losses
+losses(losses == 0) = NaN;
+
+% Find minimum loss
+[~, idx] = min(losses(:));
 [I_row, I_col] = ind2sub(size(losses), idx);
 
+% Find optimal parameters based on minimal loss
 k = I_row;
 dist = distances{I_col};
 
