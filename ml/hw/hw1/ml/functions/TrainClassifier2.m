110a4d921e51ffff21381cbcb6a66f51898a1853
Jim Leach <jimmy22theave@gmail.com>
2016-03-03 12:39:02 +0000

    Add iterating over distance metrics

--- ml/hw/hw1/ml/functions/TrainClassifier2.m
+++ ml/hw/hw1/ml/functions/TrainClassifier2.m
@@ -1,7 +1,28 @@
 function parameters = TrainClassifier2(input, output)
 
-fit = fitcknn(input, output, ...
-              'Standardize', 1, ...
-              'CrossVal', 'on');
+% Set up distance metrics
+distances = {'cityblock', 'chebychev', 'euclidean', 'minkowski'};
 
-parameters = fit;
+% Iterate over distance metrics
+for j = 1:4
+  % Iterate over values of k to find the best model
+  for i = 1:30
+    fitknn = fitcknn(input, output, ...
+                     'NumNeighbors', i,...
+                     'CrossVal', 'on',...
+                     'Standardize', 1,...
+                     'Distance', distances{j});
+    loss = kfoldLoss(fitknn);
+    losses(i, j) = loss;
+  end
+end;
+
+[min, idx] = min(losses(:));
+[I_row, I_col] = ind2sub(size(losses), idx);
+
+k = I_row;
+dist = distances{I_col};
+
+parameters = fitcknn(input, output, 'NumNeighbors', k,...
+                      'Standardize', 1,...
+                      'Distance', distances{I_col});
