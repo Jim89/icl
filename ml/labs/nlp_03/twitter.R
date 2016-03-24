296f3066bd467db34263e2f98b2a8dea9c19c040
Jim Leach <jimmy22theave@gmail.com>
2016-03-24 08:22:25 +0000

    Add code snippets from in-class exercises

---
+++ ml/labs/nlp_03/twitter.R
@@ -0,0 +1,45 @@
+
+# Step 0 - prep env -------------------------------------------------------
+
+library(quanteda)
+library(twitteR)
+
+
+# Step 1 - set up twitter access and get data -----------------------------
+key <- "iagxJa3GhzqsrSU0Lqta7uBpA"
+cons_secret <- "PzGoVxz4KwkyIUsmcOwBwCEnJ7jK5XHp7JxOxsHWQWdTYe8RKE"
+token <- "1320130406-bMoefPS3yd71l0D51qtTQxSU3sy7dQlS6uG8CCQ"
+access_secret <- "9j1v6dlcr3E6flLJFuYRAXiJINK4sswqqek7BenXJv6P2"
+
+setup_twitter_oauth(key, cons_secret, token, access_secret)
+
+tw <- userTimeline("realDonaldTrump", n = 3000, includeRts = TRUE)
+
+
+# Step 2 - Convert and clean data -----------------------------------------
+twDF <- twListToDF(tw)
+
+twCorpus <- corpus(twDF$text, docvars = twDF[, -which(names(twDF) == "text")])
+
+twCorpus[["device"]] <- ifelse(grepl("Android", twCorpus[["statusSource"]]), "Android",
+                               ifelse(grepl("iPhone", twCorpus[["statusSource"]]), "iPhone",
+                                    ifelse(grepl("Web Client", twCorpus[["statusSource"]]), "Web", NA)))
+
+# Remove day to get just time
+time.ct <- as.POSIXct(twCorpus[["created"]])
+offset <- -5
+twCorpus[["time.hour"]] <- as.POSIXlt(time.ct)$hour + offset + as.POSIXlt(time.ct)$min/60
+
+twCorpus[["time.roundhour"]] <- round(twCorpus[["time.hour"]])                                      
+
+# Produce a greouped dfm by time
+twDfm <- dfm(twCorpus, what = "fasterword",
+             ignoredFeatures = c("@*", "#*", "*.*", "http*"))
+
+# Weight the dfm - ie find the relative frequence of each word in each tweet
+twDfmWeighted <- weight(twDfm, "relFreq")
+
+
+
+
+
