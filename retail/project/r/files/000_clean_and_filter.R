3ff1438900b0885ed3ffa55630d245c75246505e
Jim Leach <jimmy22theave@gmail.com>
2016-03-31 17:43:07 +0100

    Code up market share over time, change in sales per store over time, and pairs plots of relationships between stores

---
+++ retail/project/r/files/000_clean_and_filter.R
@@ -0,0 +1,50 @@
+# Step 0 - prepare working environment------------------------------------------
+
+# Step 1 - read coffee ---------------------------------------------------------
+coffee <- read.csv("./data/InstantCoffee.csv", 
+                   encoding = "latin1", 
+                   stringsAsFactors = FALSE) %>% 
+          as_data_frame()
+
+colnames(coffee) <- coffee %>% colnames() %>% tolower()
+
+# Step 2 - filter and clean ----------------------------------------------------
+coffee_clean <- coffee %>% # Take coffee data then
+                # Filter it
+                filter(shop_desc %in% c("1TESCO", # Only take certain shops
+                                        "2ASDA", 
+                                        "3SAINSBURYS", 
+                                        "4MORRISONS", 
+                                        "7DICSOUNTERS Aldi", 
+                                        "7DISCOUNTERS Lidl")
+                       & # And
+                       sub_cat_name %in% c("Granules", # Only take certain coffees
+                                           "Freeze Dried",
+                                           "Decaf Freeze Dried", 
+                                           "Micro Ground")) %>% # then...
+                mutate(shop_desc_clean = shop_desc %>% # add cleaner shop name
+                                          substring(2) %>% 
+                                          tolower() %>% 
+                                          gsub("discounters ","", .) %>% 
+                                          gsub("aldi|lidl", "aldi & lidl", .),
+                       # Add identifier field for discounted purchases
+                       promo_price = ifelse(grepl("p off", epromdesc), 1, 0),
+                       promo_units = ifelse(!grepl("p off", epromdesc) & 
+                                            !grepl("No Promotion", epromdesc), 1, 0),
+                       # Add price field
+                       price = netspend/packs)
+
+# Step 3 - create ID -----------------------------------------------------------
+# Create transaction ID
+trans_id <- coffee_clean %>% 
+            select(relweek, day, house, shop_desc_clean) %>% 
+            distinct() %>% 
+            mutate(transaction_id = row_number())
+
+# Add back to data
+coffee_clean <- coffee_clean %>% left_join(trans_id)
+
+# Step 4 - clean up ------------------------------------------------------------
+rm(coffee, trans_id)
+gc(verbose = FALSE)
+
--- retail/project/r/misc/explorations.R
+++ retail/project/r/misc/explorations.R
@@ -4,40 +4,31 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
+library(tidyr)
+library(GGally)
+
+theme_jim <-  theme(legend.position = "bottom",
+                    axis.text.y = element_text(size = 16, colour = "black"),
+                    axis.text.x = element_text(size = 16, colour = "black"),
+                    legend.text = element_text(size = 16),
+                    legend.title = element_text(size = 16),
+                    title = element_text(size = 16),
+                    strip.text = element_text(size = 16, colour = "black"),
+                    strip.background = element_rect(fill = "white"),
+                    panel.grid.minor.x = element_blank(),
+                    panel.grid.major.x = element_line(colour = "grey", linetype = "dotted"),
+                    panel.grid.minor.y = element_line(colour = "lightgrey", linetype = "dotted"),
+                    panel.grid.major.y = element_line(colour = "grey", linetype = "dotted"),
+                    panel.margin.y = unit(0.1, units = "in"),
+                    panel.background = element_rect(fill = "white", colour = "lightgrey"),
+                    panel.border = element_rect(colour = "black", fill = NA))
 
 # Step 1 - get data -------------------------------------------------------
 
-coffee <- read.csv("./data/InstantCoffee.csv",
-                   stringsAsFactors = FALSE) %>% as_data_frame()
-colnames(coffee) <- colnames(coffee) %>% tolower() %>% stringr::str_trim()
+source("./project/r/files/000_clean_and_filter.R")
 
-
-coffee_clean <- coffee %>% # Take coffee data then
-                # Filter it
-                filter(shop_desc %in% c("1TESCO", # Only take certain shops
-                                        "2ASDA", 
-                                        "3SAINSBURYS", 
-                                        "4MORRISONS", 
-                                        "7DICSOUNTERS Aldi", 
-                                        "7DISCOUNTERS Lidl")
-                       & # And
-                         sub_cat_name %in% c("Granules", # Only take certain coffees
-                                             "Freeze Dried",
-                                             "Decaf Freeze Dried", 
-                                             "Micro Ground")) %>% # then...
-                mutate(shop_desc_clean = shop_desc %>% # add cleaner shop name
-                         substring(2) %>% 
-                         tolower() %>% 
-                         gsub("discounters ","", .) %>% 
-                         gsub("aldi|lidl", "aldi & lidl", .),
-                       # Add identifier field for discounted purchases
-                       promo_price = ifelse(grepl("p off", epromdesc), 1, 0),
-                       promo_units = ifelse(!grepl("p off", epromdesc) & 
-                                              !grepl("No Promotion", epromdesc), 1, 0),
-                       # Add price field
-                       price = netspend/packs)
-
-# Step 2 - sales over time ------------------------------------------------
+# Step 2 - plots over time ------------------------------------------------
 sales_over_time <- coffee_clean %>% 
                     group_by(relweek) %>% 
                     summarise(total_packs = sum(packs)) %>% 
@@ -51,6 +42,106 @@ sales_over_time <- coffee_clean %>%
                                   colour = shop_desc_clean)) +
                     scale_colour_brewer(palette = "Dark2", type = "qual")
 
+
+prop_sales_over_time <- coffee_clean %>% 
+                        group_by(relweek, shop_desc_clean) %>% 
+                        summarise(total_packs = sum(packs),
+                                  total_vol = sum(volume)) %>% 
+                        group_by(relweek) %>% 
+                        mutate(prop = total_packs / sum(total_packs),
+                               prop_vol = total_vol / sum(total_vol)) %>% 
+                        ggplot(aes(x = relweek, y = prop)) +
+                        geom_area(aes(fill = shop_desc_clean), colour = "white") +
+                        scale_fill_brewer(palette = "Dark2", type = "qual") +
+                        theme_jim
+                  
+delta_sales <- coffee_clean %>% 
+                group_by(relweek, shop_desc_clean) %>% 
+                summarise(total_packs = sum(packs)) %>% 
+                ungroup() %>% 
+                arrange(shop_desc_clean, relweek) %>% 
+                group_by(shop_desc_clean) %>% 
+                mutate(prev_packs = lag(total_packs),
+                       diff = total_packs - prev_packs) %>% 
+                ggplot(aes(x = relweek, y = diff, group = shop_desc_clean)) +
+                geom_line(aes(colour = shop_desc_clean), size = 1) +
+                scale_fill_brewer(palette = "Dark2", type = "qual") +
+                theme_jim
+  
+
+lowerFn <- function(data, mapping, method = "lm", ...) {
+  p <- ggplot(data = data, mapping = mapping) +
+    geom_point() +
+    geom_smooth(method = method, color = "blue", ...)
+  p
+}
+
+sales_reln <- coffee_clean %>% 
+              group_by(relweek, shop_desc_clean) %>% 
+              summarise(total_packs = sum(packs)) %>% 
+              ungroup() %>% 
+              arrange(shop_desc_clean, relweek) %>% 
+              group_by(shop_desc_clean) %>% 
+              mutate(prev_packs = lag(total_packs),
+                     diff = total_packs - prev_packs) %>% 
+              select(relweek, shop_desc_clean, total_packs) %>% 
+              spread(shop_desc_clean, total_packs) %>% 
+              slice(-1) %>% 
+              select(-relweek) %>% 
+              setNames(c("discounters", "asda", "morri", "sains", "tesco")) %>% 
+    ggpairs(lower = list(continuous = wrap(lowerFn, method = "lm")),
+            upper = list(continuous = wrap(lowerFn, method = "lm")),
+            title = "Relationship in total sales")
+
+sales_diff_reln <- coffee_clean %>% 
+                  group_by(relweek, shop_desc_clean) %>% 
+                  summarise(total_packs = sum(packs)) %>% 
+                  ungroup() %>% 
+                  arrange(shop_desc_clean, relweek) %>% 
+                  group_by(shop_desc_clean) %>% 
+                  mutate(prev_packs = lag(total_packs),
+                         diff = total_packs - prev_packs) %>% 
+                  select(relweek, shop_desc_clean, diff) %>% 
+                  spread(shop_desc_clean, diff) %>% 
+                  slice(-1) %>% 
+                  select(-relweek) %>% 
+                  setNames(c("discounters", "asda", "morri", "sains", "tesco")) %>% 
+                  ggpairs(lower = list(continuous = wrap(lowerFn, method = "lm")),
+                          upper = list(continuous = wrap(lowerFn, method = "lm")),
+                          title = "Relationships of weekly sales changes")
+
+
+shares_reln <- coffee_clean %>% 
+                group_by(relweek, shop_desc_clean) %>% 
+                summarise(total_packs = sum(packs),
+                          total_vol = sum(volume)) %>% 
+                group_by(relweek) %>% 
+                mutate(prop = total_packs / sum(total_packs),
+                       prop_vol = total_vol / sum(total_vol)) %>% 
+                select(relweek, shop_desc_clean, prop) %>% 
+                spread(shop_desc_clean, prop) %>% 
+                slice(-1) %>% 
+                select(-relweek) %>% 
+                setNames(c("discounters", "asda", "morri", "sains", "tesco")) %>% 
+                ggpairs(lower = list(continuous = wrap(lowerFn, method = "lm")),
+                        upper = list(continuous = wrap(lowerFn, method = "lm")),
+                        title = "Relationships of weekly market shares")
+
+
+
+# Step 3 - Write plots to file --------------------------------------------
 ggsave("./project/vis/rough/sales_over_time.svg", sales_over_time)
-                  
-                  
+ggsave("./project/vis/rough/delta_sales.svg", delta_sales)
+ggsave("./project/vis/rough/prop_sales_over_time.svg", prop_sales_over_time)
+
+svg("./project/vis/rough/sales_diff_reln.svg")
+sales_diff_reln
+dev.off()
+
+svg("./project/vis/rough/sales_reln.svg")
+sales_reln
+dev.off()
+
+svg("./project/vis/rough/shares_reln.svg")
+shares_reln
+dev.off()
