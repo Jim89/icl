3b2bb21da65de0f610d839e03e9bfebb00254be8
Jim Leach <jimmy22theave@gmail.com>
2016-04-01 15:26:50 +0100

    Incorporate simple choice model in to build

---
+++ retail/project/r/files/101_choice_models.R
@@ -0,0 +1,22 @@
+# Step 0 - prep env -------------------------------------------------------
+# Function to fit choice model for difference customer types
+fit_choice_model <- function(custs) {
+
+# Set up data
+choice_data <- mlogit.data(coffee_long %>% filter(cust_type == custs),
+                           choice = "choice",
+                           shape = "long",
+                           alt.var = "shop",
+                           chid.var = "transaction_id",
+                           id.var = "house")
+
+choice_fit <- mlogit(choice ~ price + promo_price + promo_units, data = choice_data)
+
+return(choice_fit)
+
+}
+
+# Step 1 - fit models -----------------------------------------------------
+
+choice_heavy <- fit_choice_model("heavy")
+choice_light <- fit_choice_model("light")
