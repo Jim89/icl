147e1d91c41d38c2f594483f7e77571dee1ef057
Jim Leach <jimmy22theave@gmail.com>
2016-04-01 09:38:48 +0100

    Generate switching matrices for light and heavy

--- retail/project/r/files/402_switching_matrices.R
+++ retail/project/r/files/402_switching_matrices.R
@@ -1,19 +1,22 @@
 # Step 0 - prep env ------------------------------------------------------------
+switching_mat <- function(custs) {
+  data <- coffee_clean %>% 
+    filter(cust_type == custs) %>% 
+    arrange(house, relweek, transaction_id) %>% 
+    select(house, relweek, transaction_id, shop_desc_clean) %>% 
+    distinct() %>% 
+    group_by(house) %>% 
+    mutate(prev_shop = lag(shop_desc_clean)) %>% 
+    ungroup() %>% 
+    arrange(house, transaction_id)
+  
+  switching_mat <- table(data$shop_desc_clean, data$prev_shop) %>% 
+    as.matrix()
+  
+  return(switching_mat)
+}  
 
 # Step 1 - Generate switching matrices -----------------------------------------
-data <- coffee_clean %>% 
-        arrange(house, relweek, transaction_id) %>% 
-        select(house, relweek, transaction_id, shop_desc_clean) %>% 
-        distinct() %>% 
-        group_by(house) %>% 
-        mutate(prev_shop = lag(shop_desc_clean)) %>% 
-        ungroup() %>% 
-        arrange(house, transaction_id)
+switch_heavy <- switching_mat("heavy")
+switch_light <- switching_mat("light")
 
-switching_mat <- table(data$shop_desc_clean, data$prev_shop) %>% 
-                  as.matrix()
-
-# Step 2 - clean up -------------------------------------------------------
-
-rm(data)
-gc(verbose = FALSE)
