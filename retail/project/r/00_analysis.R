627f901b658a92c8ca7bba9da0c1ec9b3f3a01b7
Jim Leach <jimmy22theave@gmail.com>
2016-04-01 08:22:26 +0100

    Add control file

---
+++ retail/project/r/00_analysis.R
@@ -0,0 +1,28 @@
+# Step 0 - prep env -------------------------------------------------------
+# Load packages
+library(dplyr)
+library(tidyr)
+library(magrittr)
+library(ggplot2)
+library(GGally)
+
+# Set up theme object
+theme_jim <-  theme(legend.position = "bottom",
+                    axis.text.y = element_text(size = 16, colour = "black"),
+                    axis.text.x = element_text(size = 16, colour = "black"),
+                    legend.text = element_text(size = 16),
+                    legend.title = element_text(size = 16),
+                    title = element_text(size = 16),
+                    strip.text = element_text(size = 16, colour = "black"),
+                    strip.background = element_rect(fill = "white"),
+                    panel.grid.minor.x = element_blank(),
+                    panel.grid.major.x = element_line(colour = "grey", linetype = "dotted"),
+                    panel.grid.minor.y = element_line(colour = "lightgrey", linetype = "dotted"),
+                    panel.grid.major.y = element_line(colour = "grey", linetype = "dotted"),
+                    panel.margin.y = unit(0.1, units = "in"),
+                    panel.background = element_rect(fill = "white", colour = "lightgrey"),
+                    panel.border = element_rect(colour = "black", fill = NA))
+
+# Step 1 - get and clean --------------------------------------------------
+
+source("./project/r/files/000_clean_and_filter.R")
