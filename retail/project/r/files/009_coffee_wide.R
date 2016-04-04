aa24a6b1b8679bb49cb9ac7bba6ccf2b760422d3
Jim Leach <jimmy22theave@gmail.com>
2016-04-04 08:04:32 +0100

    Create wide data for elasticitiy modelling

---
+++ retail/project/r/files/009_coffee_wide.R
@@ -0,0 +1,40 @@
+# Step 0 - prepare environment -------------------------------------------------
+# In-line function definition used in this script only
+# This function takes the coffee data, filters to just one customer type (e.g.
+# heavy or light), aggregates to a weekly level and then spreads the data
+# in to a wide form suitable for modelling
+widen <- function(data, cust_status = 0) {
+  # Filter and widen at brand level
+  shop_level <- coffee_clean %>% 
+    filter(cust_type %in% cust_status) %>% 
+    group_by(relweek, day, shop_desc_clean) %>% 
+    summarise(sales = sum(packs),
+              price = mean(price),
+              promo_sales_price = sum(promo_price),
+              promo_sales_units = sum(promo_units)) %>% 
+    mutate(promo_cost = promo_sales_price/sales,
+           promo_units = promo_sales_units/sales) %>% 
+    select(-promo_sales_price, -promo_sales_units) %>% 
+    gather(variable, value, -(relweek:shop_desc_clean)) %>%
+    mutate(shop_desc_clean = ifelse(shop_desc_clean == "aldi & lidl", 
+                                    "aldi&lidl", 
+                                    shop_desc_clean)) %>% 
+    unite(temp, shop_desc_clean, variable, sep = "_") %>% 
+    spread(temp, value) %>% 
+    ungroup()
+  
+  colnames(shop_level) <- colnames(shop_level) %>% gsub(" ", "_", .) %>% tolower()
+
+    return(shop_level)
+}
+
+# Step 1 - perform the spread --------------------------------------------------
+# Filter and spread
+coffee_wide <- widen(coffee_clean, c("heavy", "light", "medium"))
+
+# Clean up missing values with mean-substitution
+for(i in 1:ncol(coffee_wide)){
+  val <- mean(coffee_wide[,i] %>% sapply(as.numeric), na.rm = TRUE)
+  coffee_wide[is.na(coffee_wide[,i]), i] <- val
+}
+
