c2cf9982e1cabada6ab64f0b2ee938bfad463631
Jim Leach <jimmy22theave@gmail.com>
2016-04-04 15:51:59 +0100

    Change light vs. heavy split to be based on avg volume per visit, not per week

--- retail/project/r/files/001_light_vs_heavy.R
+++ retail/project/r/files/001_light_vs_heavy.R
@@ -3,18 +3,18 @@
 
 # Step 1 - aggregate data ------------------------------------------------------
 house_summary <- coffee_clean %>% 
-                group_by(relweek, house) %>% 
+                group_by(visit_id, house) %>% 
                 summarise(visits = n(),
                   total_vol = sum(volume)) %>% 
                 ungroup() %>% 
                 group_by(house) %>% 
-                summarise(avg_weekly_vol = mean(total_vol))
+                summarise(avg_visit_vol = mean(total_vol))
 
 # Step 2 - classify into light vs heavy ----------------------------------------
-quartiles <- quantile(house_summary$avg_weekly_vol)
+quartiles <- quantile(house_summary$avg_visit_vol)
 light_vs_heavy <- house_summary %>% 
-                  mutate(cust_type = ifelse(avg_weekly_vol <= quartiles[2], "light",
-                                      ifelse(avg_weekly_vol >= quartiles[4], "heavy", 
+                  mutate(cust_type = ifelse(avg_visit_vol <= quartiles[2], "light",
+                                      ifelse(avg_visit_vol >= quartiles[4], "heavy", 
                                              "medium"))) %>% 
                   select(house, cust_type)
 
