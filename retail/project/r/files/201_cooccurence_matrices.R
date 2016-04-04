f73586c93a6d2dc4766fb442f6309ee8808114d5
Jim Leach <jimmy22theave@gmail.com>
2016-04-04 11:45:31 +0100

    Add new objects to clean up

--- retail/project/r/files/201_cooccurence_matrices.R
+++ retail/project/r/files/201_cooccurence_matrices.R
@@ -1,29 +1,25 @@
 # Step 0 - prepare env ---------------------------------------------------------
-
-
-# Step 1 - prepare data --------------------------------------------------------
 # Create function to find unique shops in each week for each house
-find_shops <- function(cust){
+find_shops <- function(custs){
   coffee_clean %>% 
-    filter(cust_type == cust) %>% 
+    filter(cust_type %in% custs) %>% 
     dplyr::select(relweek, house, shop_desc_clean) %>% 
     distinct() %>% 
     group_by(house, relweek) %>% 
     mutate(prev_shop = lag(shop_desc_clean))
 }
 
+# Step 1 - prepare data --------------------------------------------------------
 # Apply function for heavy and light users
-stores_per_week_light <- find_shops("light")
-stores_per_week_heavy <- find_shops("heavy")
+stores_per_week <- find_shops(custs = c("heavy", "light"))
 
 # Step 2 - reshape to co-occurence ---------------------------------------------
 # Get co-occurenc matrix (may be inefficient on large data)
-cooccurence_light <- table(stores_per_week_light$shop_desc_clean, stores_per_week_light$prev_shop)
-cooccurence_heavy <- table(stores_per_week_heavy$shop_desc_clean, stores_per_week_heavy$prev_shop)
+cooccurence <- table(stores_per_week$shop_desc_clean, stores_per_week$prev_shop)
 
 # Step 3 - clean up -------------------------------------------------------
 
-rm(stores_per_week_light, stores_per_week_heavy)
+rm(stores_per_week, find_shops)
 gc()
 
 
