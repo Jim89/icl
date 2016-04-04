a6999e8210873a11b88b872c2b2c4ee77cf0e639
Jim Leach <jimmy22theave@gmail.com>
2016-04-04 10:01:48 +0100

    Remove medium-user model and add cleanup

--- retail/project/r/files/102_traffic_models.R
+++ retail/project/r/files/102_traffic_models.R
@@ -6,7 +6,7 @@ ref_prices <- coffee_clean %>%
                         max_price = max(price))  
   
 fit <-  coffee_clean %>% 
-        filter(cust_type == custs) %>% 
+        filter(cust_type %in% custs) %>% 
         group_by(relweek, day, shop_desc_clean, brand_clean) %>% 
         summarise(visits = n(),
                   avg_price = mean(price),
@@ -14,15 +14,17 @@ fit <-  coffee_clean %>%
                   promo_units = mean(promo_units)) %>% 
         left_join(ref_prices) %>% 
         ungroup() %>% 
-        mutate(shop_desc_clean = as.factor(shop_desc_clean),
-               brand_clean = as.factor(brand_clean)) %>% 
-        zerotrunc(visits ~ shop_desc_clean + brand_clean + avg_price + promo_price + promo_units + min_price + max_price,
+        mutate(shop = as.factor(shop_desc_clean),
+               brand = as.factor(brand_clean)) %>% 
+        zerotrunc(visits ~ shop + brand + avg_price + promo_price + promo_units + min_price + max_price,
                   data = ., dist = "poisson")
 }
 
 # Step 1 - fit models -----------------------------------------------------
 traffic_heavy <- model_traffic("heavy")
-traffic_medium <- model_traffic("medium")
 traffic_light <- model_traffic("light")
 
+# Step 2 - clean up -------------------------------------------------------
 
+rm(model_traffic)
+gc(verbose = FALSE)
