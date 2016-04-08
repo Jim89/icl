394e9c36e8e45e7c379430cb25b0afe51df1a2f0
Jim Leach <jimmy22theave@gmail.com>
2016-04-08 11:19:40 +0100

    Create facetted repeat purchase rate plot

--- retail/project/r/files/503_vis_repeat_purchase_rate.R
+++ retail/project/r/files/503_vis_repeat_purchase_rate.R
@@ -29,3 +29,30 @@ make_repeat_purchase_plot <- function(data) {
 repeat_purchase_rate_h <- make_repeat_purchase_plot(loyalties_h)
 repeat_purchase_rate_m <- make_repeat_purchase_plot(loyalties_m)
 repeat_purchase_rate_l <- make_repeat_purchase_plot(loyalties_l)
+repeat_purchase_rate_all <- cbind(loyalties_h, loyalties_m, loyalties_l) %>% 
+  data.frame() %>% 
+  add_rownames() %>% 
+  rename(shop = rowname) %>% 
+  gather(user, loyalty, -shop) %>% 
+  mutate(user = as.character(user),
+         user = ifelse(grepl("_h", user), "1 - Heavy", user),
+         user = ifelse(grepl("_m", user), "2 - Medium", user),
+         user = ifelse(grepl("_l", user), "3 - Light", user)) %>% 
+  ggplot(aes(x = toproper(shop), y = loyalty)) +
+  geom_segment(aes(xend = toproper(shop)), yend = 0.2, linetype = "dashed",
+               size = 0.5) +
+  geom_point(size = 10, aes(colour = toproper(shop))) +
+  facet_grid(user ~ .) +
+  scale_y_continuous(limits = c(.2, .8), labels = scales::percent) +
+  # scale_x_discrete(limits = data %>% sort() %>% names() %>% toproper())
+  guides(colour = guide_legend(title = "")) + 
+  geom_hline(yintercept = 0.2, colour = "grey") +
+  scale_colour_brewer(palette = "Dark2", type = "qual") +
+  xlab("") +
+  ylab("Repeat purchases (as percentage of total purchases)") +
+  coord_flip() +
+  theme_jim +
+  theme(panel.grid.minor.x = element_blank(),
+        panel.grid.major.x = element_line(colour = "grey", linetype = "dotted"),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_blank())
