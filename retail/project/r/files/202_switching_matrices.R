ac0822ac3b5604f8dbc8b9a8293ca2b654079d0d
Jim Leach <jimmy22theave@gmail.com>
2016-04-08 11:18:14 +0100

    Create overall switching mat

--- retail/project/r/files/202_switching_matrices.R
+++ retail/project/r/files/202_switching_matrices.R
@@ -30,15 +30,30 @@ get_loyalties <- function(mat) {
   return(loy)
 }
 
+get_switch_prop <- function(mat){
+  total <- sum(mat)
+  diag(mat) <- 0 
+  switched <- sum(mat)
+  return(switched/total)
+}
+
 # Step 1 - Generate switching matrices -----------------------------------------
 switch_mat_l <- switching_mat(c("light"))
 switch_mat_m <- switching_mat(c("medium"))
 switch_mat_h <- switching_mat(c("heavy"))
+switch_mat_all <- switching_mat(c("heavy", "medium", "light"))
 
 # Compute repeat-purchase loyalty
 loyalties_l <- get_loyalties(switch_mat_l)
 loyalties_m <- get_loyalties(switch_mat_m)
 loyalties_h <- get_loyalties(switch_mat_h)
+loyalties_all <- get_loyalties(switch_mat_all)
+
+# Prop switching
+switch_l <- get_switch_prop(switch_mat_l)
+switch_m <- get_switch_prop(switch_mat_m)
+switch_h <- get_switch_prop(switch_mat_h)
+switch_all <- get_switch_prop(switch_mat_all)
 
 # Step 2 - clean up -------------------------------------------------------
 
