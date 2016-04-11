59d012afcd5b192dd34136a5ba8d589b051d8f08
Jim Leach <jimmy22theave@gmail.com>
2016-04-11 15:45:59 +0100

    Reduce point size

--- retail/project/r/files/503_vis_repeat_purchase_rate.R
+++ retail/project/r/files/503_vis_repeat_purchase_rate.R
@@ -9,7 +9,7 @@ make_repeat_purchase_plot <- function(data) {
     ggplot(aes(x = shop, y = loyalty)) +
     scale_y_continuous(limits = c(.0, .8), labels = scales::percent) +
     geom_segment(aes(xend = shop), yend = 0, linetype = "dashed") +
-    geom_point(size = 15, aes(colour = shop)) +
+    geom_point(size = 7.5, aes(colour = shop)) +
     guides(colour = "none") + 
     scale_x_discrete(limits = data %>% sort() %>% names() %>% toproper()) +
     geom_hline(yintercept = 0.0, colour = "grey") +
