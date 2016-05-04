24314f2616b7949118125b57b32ec6de5c91ae34
Jim Leach <jimmy22theave@gmail.com>
2016-05-04 10:04:56 +0100

    Split up dfm creation in to discrete steps

---
+++ ml/project/r/files/20_tokenise_texts.R
@@ -0,0 +1,22 @@
+
+# Step 0 - prep env -------------------------------------------------------
+
+
+# Step 1 - create corpus and tokenise -------------------------------------
+# Use emails clean data and remove "to" to get unique documents
+uniques <- emails_clean %>% 
+    select(-to) %>% 
+    distinct()
+
+# Build a corpus with quanteda
+hil_corp <- corpus(v$body, docvars = uniques[, c(2:6)],
+                   docnames = uniques$DocNumber)
+
+# Tokenise and remove stopwords from the corpus
+hil_tok <- quanteda::tokenize(hil_corp, removeNumbers = T, removePunct = T, 
+                              removeSeparators = T, removeHyphens = T) %>% 
+    removeFeatures(c(stopwords("english"), "will"))
+
+# Step 2 - clean up -------------------------------------------------------
+rm(uniques)
+gc()
---
+++ ml/project/r/files/21_apply_dictionary.R
@@ -0,0 +1,31 @@
+
+# Step 0 - prep env -------------------------------------------------------
+
+
+# Step 1 - apply LIWC dictionary to corpus --------------------------------
+# Apply the dictionary
+hil_dfm_liwc <- dfm(hil_tok, dictionary = liwc)
+
+
+# Step 2 - extract further features for analysis --------------------------
+# Emotional content
+emot <- c("posemo", "negemo", "anx", "anger", "sad")
+hil_dfm_senti <- hil_dfm_liwc[, emot]
+hil_dfm_senti <- weight(hil_dfm_senti, type = "relFreq")
+
+
+# Past vs. present vs. future
+tenses <- c("focuspast", "focuspresent", "focusfuture")
+hil_dfm_focus <- hil_dfm_liwc[ , tenses]
+hil_dfm_focus <- weight(hil_dfm_focus, type = "relFreq")
+
+# Word use
+pos_tags <- c("pronoun", "article", "prep", "auxverb", "adverb", "conj", 
+              "verb", "adj", "netspeak")
+hil_dfm_pos <- hil_dfm_liwc[ , pos_tags]
+hil_dfm_pos <- weight(hil_dfm_pos, type = "relFreq")
+
+
+# Clean up ----------------------------------------------------------------
+rm(emot, tenses, pos_tags)
+gc()
