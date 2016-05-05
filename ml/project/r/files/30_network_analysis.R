9eba9691ce820febd43d12d92b49015eb24c83c1
Jim Leach <jimmy22theave@gmail.com>
2016-05-05 16:29:25 +0100

    Add network analysis code to repo

--- ml/project/r/files/30_network_analysis.R
+++ ml/project/r/files/30_network_analysis.R
@@ -1,7 +1,39 @@
-# Summarise to/from data in to edgelist
+# Step 0 - prep env -------------------------------------------------------
+
+
+# Step 1 - prepare edgelist and create igraph -----------------------------
+# Summarise to/from data in to edgelist and grab only those pairs who sent/received
+# more than 1 email
 edgelist <- to_from %>% 
+    left_join(emails_clean) %>% 
+    select(DocNumber, from, to, redacted) %>% 
     group_by(from, to) %>% 
-    summarise(emails = n()) 
+    summarise(emails = n(),
+              prop_redacted = mean(redacted)) %>% 
+    na.omit() %>% 
+    filter(emails > 1)
+
+
+# Create the igraph object for further analysis
+graph <- graph_from_data_frame(edgelist)
+
+
+# Step 2 - perform some analysis on the graph -----------------------------
+# Centrality
+centralities <- eigen_centrality(graph, weights = E(graph)$emails)
+
+# Betweenness
+betweens <- betweenness(graph, weights = E(graph)$emails)
+
+# Closeness
+closes <- closeness(graph, weights = E(graph)$emails)
+
+
+# Step 3 - try to find clusters in the graph ------------------------------
+
+cl_opt <- cluster_optimal(graph, weights = E(graph)$emails)
+cl_walk <- cl_walktraph(graph, weights = E(graph)$emails)
+
 
 # Create a function to plot the network
 create_network_plot <- function(data, messages = 0, normalise = TRUE) {
@@ -12,17 +44,17 @@ create_network_plot <- function(data, messages = 0, normalise = TRUE) {
     graph <- graph_from_data_frame(data)
     wc <- cluster_walktrap(graph)
     members <- membership(wc)
-
+    
     # Convert to a networkD3 data structure for D3 plotting
     networks <- igraph_to_networkD3(graph, group = members)
-
+    
     # Add link weights and optionally normalise
     networks$links$value <- data$emails
     
     if (normalise == TRUE) {
-    networks$links$value <- (networks$links$value - mean(networks$links$value)) / sd(networks$links$value)
+        networks$links$value <- (networks$links$value - mean(networks$links$value)) / sd(networks$links$value)
     }
-
+    
     # Plot the network
     forceNetwork(Links = networks$links,
                  Nodes = networks$nodes,
