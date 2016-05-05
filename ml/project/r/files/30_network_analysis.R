e6a6433a344536a528a22d54008e50de06664a50
Jim Leach <jimmy22theave@gmail.com>
2016-05-05 16:52:30 +0100

    Clean up code

--- ml/project/r/files/30_network_analysis.R
+++ ml/project/r/files/30_network_analysis.R
@@ -11,7 +11,7 @@ edgelist <- to_from %>%
     summarise(emails = n(),
               prop_redacted = mean(redacted)) %>% 
     na.omit() %>% 
-    filter(emails > 1)
+    filter(emails > 0)
 
 
 # Create the igraph object for further analysis
@@ -31,45 +31,28 @@ closes <- closeness(graph, weights = E(graph)$emails)
 
 # Step 3 - try to find clusters in the graph ------------------------------
 
-cl_opt <- cluster_optimal(graph, weights = E(graph)$emails)
-cl_walk <- cl_walktraph(graph, weights = E(graph)$emails)
+cl_walk <- cluster_walktrap(graph)
+members <- membership(cl_walk)
 
+# Convert to a networkD3 data structure for D3 plotting
+networks <- igraph_to_networkD3(graph, group = members)
 
-# Create a function to plot the network
-create_network_plot <- function(data, messages = 0, normalise = TRUE) {
-    # Filter the data
-    data <- data %>% filter(emails >= messages)
-    
-    # Set up the graph object and make some clusters (for colour for now)
-    graph <- graph_from_data_frame(data)
-    wc <- cluster_walktrap(graph)
-    members <- membership(wc)
-    
-    # Convert to a networkD3 data structure for D3 plotting
-    networks <- igraph_to_networkD3(graph, group = members)
-    
-    # Add link weights and optionally normalise
-    networks$links$value <- data$emails
-    
-    if (normalise == TRUE) {
-        networks$links$value <- (networks$links$value - mean(networks$links$value)) / sd(networks$links$value)
-    }
-    
-    # Plot the network
-    forceNetwork(Links = networks$links,
-                 Nodes = networks$nodes,
-                 colourScale = JS("d3.scale.category20()"),
-                 Source = "source",
-                 Target = "target",
-                 Value = "value",
-                 NodeID = "name",
-                 Group = "group",
-                 charge = -250,
-                 linkColour = "grey",
-                 opacity = 1,
-                 legend = F,
-                 bounded = F,
-                 zoom = TRUE)
-}    
+# Add link weights and optionally normalise
+networks$links$value <- E(graph)$emails %>% normalise
 
-create_network_plot(edgelist, messages = 1, normalise = TRUE)
+# Plot the network
+forceNetwork(Links = networks$links,
+             Nodes = networks$nodes,
+             colourScale = JS("d3.scale.category20()"),
+             Source = "source",
+             Target = "target",
+             Value = "value",
+             NodeID = "name",
+             Group = "group",
+             charge = -250,
+             linkColour = "grey",
+             opacity = 1,
+             legend = F,
+             bounded = F,
+             zoom = TRUE)
+
