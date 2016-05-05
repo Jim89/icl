305f263dd4d802f6bf91afbe338c085ec5f08020
Jim Leach <jimmy22theave@gmail.com>
2016-05-05 15:54:47 +0100

    Add code to create 'raw' and weighted dfm of just features without LIWC

---
+++ ml/project/r/files/20_hil_dfm.R
@@ -0,0 +1,30 @@
+
+# Step 0 - prep env -------------------------------------------------------
+
+
+# Step 1 - create dfm from corpus -----------------------------------------
+# Create the bare dfm
+hil_dfm <- dfm(hil_tok)
+topfeatures(hil_dfm, 20)
+
+# Create a relative-frequency weighted dfm
+hil_dfm_rel <- weight(hil_dfm, type = "relFreq")
+topfeatures(hil_dfm_rel, 20)
+
+# Step 2 - extract features for further analysis --------------------------
+# Count the appearances of features (words) across all documents
+feature_counts <- colSums(hil_dfm) %>% 
+                    as.matrix(ncol = 1) %>% 
+                    data.frame(count = ., row.names = rownames(.)) %>% 
+                    add_rownames("feature") %>% 
+                    arrange(-count)
+
+# Find only those words that are not too common, but not too rare
+to_keep <- feature_counts %>% filter(count > 5 & count <= 500) %>% .$feature 
+
+# Only keep those words
+hil_dfm_words <- hil_dfm[, to_keep]            
+
+# Clean up ----------------------------------------------------------------
+rm(feature_counts, to_keep)
+gc()
