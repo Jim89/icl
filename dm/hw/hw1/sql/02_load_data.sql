9232e69f27b70e3cdc409b481b55fe4aad0545ad
Jim Leach <jimmy22theave@gmail.com>
2016-05-09 19:12:36 +0100

    Update ignore for data and add scripts to repo

---
+++ dm/hw/hw1/sql/02_load_data.sql
@@ -0,0 +1,15 @@
+﻿COPY contacts(cust_id, contact_date, contact_type)
+FROM
+	'C:\Users\Jleach1\Documents\icl\dm\data\hw1\DMEFExtractContactsV01.CSV'
+WITH CSV HEADER
+
+COPY lines(cust_id, ordernum, order_date, linedollars, gift, recipnum)
+FROM
+	'C:\Users\Jleach1\Documents\icl\dm\data\hw1\DMEFExtractLinesV01.CSV'
+WITH CSV HEADER
+
+COPY orders
+FROM
+	'C:\Users\Jleach1\Documents\icl\dm\data\hw1\DMEFExtractOrdersV01.CSV'
+WITH CSV HEADER
+
