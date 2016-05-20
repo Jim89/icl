974862aeac397633159909a826f8fd0437e1765b
Jim Leach <jimmy22theave@gmail.com>
2016-05-20 11:48:15 +0100

    Change colour scheme to cat10, add radius calculation basd on nodesize, increase font size for names

--- ml/project/r/functions/plot_network.R
+++ ml/project/r/functions/plot_network.R
@@ -11,17 +11,26 @@ plot_network <- function(graph_object, clusters = "walk") {
 # Add link weights and optionally normalise
 networks$links$value <- E(graph_object)$emails %>% normalise
 
+# Add nodesize based on emails sent
+networks$nodes <- networks$nodes %>% 
+    left_join(emails_clean %>% group_by(from) %>% summarise(sent = n()), 
+              by = c("name" = "from")) %>% 
+    mutate(sent = ifelse(is.na(sent), 1, sent + 1))
+
 # Plot the network
 forceNetwork(Links = networks$links,
              Nodes = networks$nodes,
-             colourScale = JS("d3.scale.category20()"),
+             colourScale = JS("d3.scale.category10()"),
              Source = "source",
              Target = "target",
              Value = "value",
              NodeID = "name",
+             Nodesize = "sent",
+             radiusCalculation = JS("Math.log(d.nodesize)+5"),
              Group = "group",
-             charge = -250,
+             charge = -1000,
              linkColour = "grey",
+             fontSize = 16,
              opacity = 1,
              legend = F,
              bounded = F,
