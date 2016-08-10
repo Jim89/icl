1ff1e6a5aaf2fcd78f391b2edc9ef77230844c45
Jim Leach <jimmy22theave@gmail.com>
2016-08-10 17:00:40 +0100

    Tidy up code

--- final_report/R/02_routes.R
+++ final_report/R/02_routes.R
@@ -1,5 +1,23 @@
-
 library(purrr)
+library(igraph)
+
+# Step 1 - create graph object --------------------------------------------
+# Chop out line and distinct
+graph_data <- links %>% 
+    distinct() %>% 
+    select(-line)
+
+# Make the graph - retain distance as edge attribute but do not weight edges
+tfl_graph <- graph_data %>% 
+    graph_from_data_frame(directed = TRUE)
+
+# Clean up
+rm(graph_data)
+
+# Find all distinct journeys, removing backwards journeys
+distinct_journeys <- journeys %>% select(start_cln, end_cln) %>% distinct()
+# distinct_journeys <- unique(t(apply(df, 1, sort))) %>% dplyr::as_data_frame()
+# names(distinct_journeys) <- c("start_cln", "end_cln")
 
 get_shortest_path <- function(node1, node2, graph = tfl_graph) {
     vertices <- V(graph)
@@ -9,23 +27,15 @@ get_shortest_path <- function(node1, node2, graph = tfl_graph) {
     return(path)
 }
 
-# Find all distinct journeys, removing backwards journeys
-distinct_journeys <- journeys %>% select(start_cln, end_cln) %>% distinct()
-# distinct_journeys <- unique(t(apply(df, 1, sort))) %>% dplyr::as_data_frame()
-# names(distinct_journeys) <- c("start_cln", "end_cln")
-
 # Get the paths
 t1 <- Sys.time()
 paths <- distinct_journeys %>% 
     # slice(1:100) %>% 
-    mutate(path = map2(start_cln, end_cln, get_shortest_path))
-Sys.time() - t1    
-    
-# Unnest the paths and create the route
-paths <- paths %>% 
+    mutate(path = map2(start_cln, end_cln, get_shortest_path)) %>% 
     unnest(path)
+Sys.time() - t1    
 
-# Set up row_number    
+# Set up routes with lead    
 routes <- paths %>% 
     group_by(start_cln, end_cln) %>% 
     mutate(to = lead(path, 1)) %>% 
